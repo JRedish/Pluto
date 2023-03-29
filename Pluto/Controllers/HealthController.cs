f7d936cbb31e147bfeaf2d3a90a5381100b829ff
Jake Redish <jakeredish@gmail.com>
2023-03-29 10:27:01 -0600

    Health controller and Mineral controller+service

---
+++ Pluto/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pluto.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    public HealthController() { }
+
+    [HttpGet]
+    public OkObjectResult IsAlive()
+    {
+        return new OkObjectResult(new { Message = "I'm Alive!" });
+    }
+}
---
+++ Pluto/Services/MineralService.cs
@@ -0,0 +1,12 @@
+namespace Pluto.Services;
+
+public class MineralService : IMineralService
+{
+    public MineralService() {
+
+    }
+
+    public string GetMineral(string name) {
+        return "I'm building it!";
+    }
+}
